956e386235c4d2c96f28be99917a27d5673cdabf
Chandler Jong <40523532+harambetron@users.noreply.github.com>
2025-08-18 00:42:57 -0400

    chore: finalize package code and fix analyzer warnings

--- lib/isa_kit.dart
+++ lib/isa_kit.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-import 'dart:math';
 
 // --- MODELS (Exported for users) ---
 
